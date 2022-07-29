99aeab8eee4c73b16d5079e31160b2a2360cffeb
tutul <tutul5162@gmail.com>
2022-07-30 01:55:33 +0600

    link asset file

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-  <meta name="copyright" content="MACode ID, https://macodeid.com/">
-
-  <title>One Health - Medical Center </title>
-
-  <link rel="stylesheet" href="{{asset('frontend/../assets/css/maicons.css')}}">
-
-  <link rel="stylesheet" href="{{asset('frontend/../assets/css/bootstrap.css')}}">
-
-  <link rel="stylesheet" href="{{asset('frontend/../assets/vendor/owl-carousel/css/owl.carousel.css')}}">
-
-  <link rel="stylesheet" href="{{asset('frontend/../assets/vendor/animate/animate.css')}}">
-
-  <link rel="stylesheet" href="{{asset('frontend/../assets/css/theme.css')}}">
-</head>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  
+    <meta name="copyright" content="MACode ID, https://macodeid.com/">
+  
+    <title>One Health - Medical Center HTML5 Template</title>
+  
+    <link rel="stylesheet" href="{{asset('../frontend/assets/css/maicons.css')}}">
+  
+    <link rel="stylesheet" href="{{asset('../frontend/assets/css/bootstrap.css')}}">
+  
+    <link rel="stylesheet" href="{{asset('../frontend/assets/vendor/owl-carousel/css/owl.carousel.css')}}">
+  
+    <link rel="stylesheet" href="{{asset('../frontend/assets/vendor/animate/animate.css')}}">
+  
+    <link rel="stylesheet" href="{{asset('../frontend/assets/css/theme.css')}}">
+  </head>
 <body>
 
   <!-- Back to top button -->
@@ -33,15 +33,15 @@
   {{-- footer part  --}}
   @include('frontend.layout.footer')
 
-<script src="{{asset('frontend/../assets/js/jquery-3.5.1.min.js')}}"></script>
+  <script src="{{asset('../frontend/assets/js/jquery-3.5.1.min.js')}}"></script>
 
-<script src="{{asset('frontend/../assets/js/bootstrap.bundle.min.js')}}"></script>
-
-<script src="{{asset('frontend/../assets/vendor/owl-carousel/js/owl.carousel.min.js')}}"></script>
-
-<script src="{{asset('frontend/../assets/vendor/wow/wow.min.js')}}"></script>
-
-<script src="{{asset('frontend/../assets/js/theme.js')}}"></script>
+  <script src="{{asset('../frontend/assets/js/bootstrap.bundle.min.js')}}"></script>
+  
+  <script src="{{asset('../frontend/assets/vendor/owl-carousel/js/owl.carousel.min.js')}}"></script>
+  
+  <script src="{{asset('../frontend/assets/vendor/wow/wow.min.js')}}"></script>
+  
+  <script src="{{asset('../frontend/assets/js/theme.js')}}"></script>
   
 </body>
 </html>
